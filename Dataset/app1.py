b196a83acb05371e5ed59db78920c9a0df7ebe57
xiaoyi0217 <lxy030217@gmail.com>
2025-04-23 21:05:31 +0800

    Update app1.py

--- Dataset/app1.py
+++ Dataset/app1.py
@@ -239,6 +239,7 @@ def insights_page():
     if st.button("Get AI-Powered Recommendations"):
         prompt = (
             "- You are an ai so be friendly, say who are you and then said about on their result.\n"
+            f"Here is the user name: {st.session_state.username}"
             "- You are a concise mental health coach.\n"
             f"- You need to say the User Social Anxiety level: {sa_cat}.\n"
             f"- You need to say the Anxiety level: {latest['Anxiety Levels (1-10)']}.\n"
